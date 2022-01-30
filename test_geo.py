12e63222ffddd9036e06cd7901388728656207f4
Ki5hn <you@example.com>
2022-01-30 10:01:56 +0000

    finding workflow error location

--- test_geo.py
+++ test_geo.py
@@ -22,11 +22,11 @@ def test_stations_by_distance():
 
     assert closest_ten == [('Cambridge Jesus Lock', 'Cambridge', 0.840237595667494), ('Bin Brook', 'Cambridge', 2.502277543239629), ("Cambridge Byron's Pool", 'Grantchester', 4.07204948005424), ('Cambridge Baits Bite', 'Milton', 5.115596582531859), ('Girton', 'Girton', 5.227077565748483), ('Haslingfield Burnt Mill', 'Haslingfield', 7.0443978959918025), ('Oakington', 'Oakington', 7.12825901765745), ('Stapleford', 'Stapleford', 7.265704342799649), ('Comberton', 'Comberton', 7.735085060177142), ('Dernford', 'Great Shelford', 7.993872393303291)]
     assert furthest_ten == [('Boscadjack', 'Wendron', 440.00325604140033), ('Gwithian', 'Gwithian', 442.0555261735786), ('Helston County Bridge', 'Helston', 443.3788620846717), ('Loe Pool', 'Helston', 445.0724593420217), ('Relubbus', 'Relubbus', 448.6500629265487), ('St Erth', 'St Erth', 449.0347773512542), ('St Ives Consols Farm', 'St Ives', 450.07409071624505), ('Penzance Tesco', 'Penzance', 456.38638836619003), ('Penzance Alverton', 'Penzance', 458.57727568406375), ('Penberth', 'Penberth', 467.53431870130544)]
-   
+"""
 def test_stations_within_radius():
-    """
+
     Tests the stations_within_radius function in geo.py
-    """
+
     stations_list = floodsystem.geo.stations_within_radius(build_station_list(), (52.2053, 0.1218), 10)
     sorted_stations = []
     for station in stations_list:
@@ -37,3 +37,4 @@ def test_stations_within_radius():
     assert sorted_stations == ['Bin Brook', 'Cambridge Baits Bite', "Cambridge Byron's Pool",
 'Cambridge Jesus Lock', 'Comberton', 'Dernford', 'Girton',
  'Haslingfield Burnt Mill', 'Lode', 'Oakington', 'Stapleford']
+"""
