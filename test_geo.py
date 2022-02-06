ef67d8f6c635e13302fe9ba6bbe7ce5368bf6f3f
Callum Halton <ch936@cam.ac.uk>
2022-02-06 15:05:48 +0000

    Adds tests for Task 1E's geo function (rivers_by_station_number)

--- test_geo.py
+++ test_geo.py
@@ -61,6 +61,7 @@ def test_stations_by_river():
     stations_by_rivername = stations_by_river(build_station_list())
     # Checks stations_by_rivername is a dict
     assert type(stations_by_rivername) == dict
+
     # Checks river sets for the 3 rivers in the demo
     expected_stations = {
         'River Aire'   : ['Airmyn', 'Apperley Bridge', 'Armley', 'Beal Weir Bridge', 'Bingley', 'Birkin Holme Washlands', 'Carlton Bridge', 'Castleford', 'Chapel Haddlesey', 'Cononley', 'Cottingley Bridge', 'Ferrybridge Lock', 'Fleet Weir', 'Gargrave', 'Kildwick', 'Kirkstall Abbey', 'Knottingley Lock', 'Leeds Crown Point', 'Leeds Crown Point Flood Alleviation Scheme', 'Leeds Knostrop Weir Flood Alleviation Scheme', 'Oulton Lemonroyd', 'Saltaire', 'Snaygill', 'Stockbridge'],
@@ -70,9 +71,29 @@ def test_stations_by_river():
     for rivername in expected_stations:
         assert sorted(stations_by_rivername[rivername]) == expected_stations[rivername]
 
-'''
+
 def test_rivers_by_station_number():
     """
     Tests the rivers_by_station_number function in geo.py
     """
-'''
+    # Test will be run for the stations in the top 20 ranks by number of rivers
+    rivers_by_station_number_20 = rivers_by_station_number(build_station_list(), 20)
+    # Tests result is list of tuples of length 2
+    assert type(rivers_by_station_number_20) == list
+    for pair in rivers_by_station_number_20:
+        assert type(pair) == tuple
+        assert len(pair) == 2
+
+    # Tests tuples returned are in order of number of stations...
+    ranks = 1
+    lastPair = rivers_by_station_number_20[0]
+    for pair in rivers_by_station_number_20[1:]:
+        assert pair[1] <= lastPair[1]
+        if pair[1] < lastPair[1]:
+            ranks += 1
+        lastPair = pair
+    # ... and that they only include the top 20 ranks
+    assert ranks == 20
+
+    # Checks rivers_by_station_number_20 against known result
+    assert rivers_by_station_number_20 == [('River Thames', 55), ('River Avon', 31), ('River Great Ouse', 30), ('River Derwent', 25), ('River Aire', 24), ('River Calder', 23), ('River Severn', 21), ('River Stour', 21), ('River Ouse', 18), ('River Colne', 18), ('River Rother', 17), ('River Trent', 16), ('River Witham', 15), ('River Nene', 15), ('River Tame', 14), ('River Wey', 14), ('River Wharfe', 13), ('River Medway', 13), ('River Don', 13), ('River Mole', 11), ('River Test', 11), ('River Itchen', 10), ('River Wye', 10), ('River Dove', 9), ('River Irwell', 9), ('River Blackwater', 9), ('River Ribble', 9), ('River Arun', 9), ('River Cherwell', 9), ('River Pinn', 8), ('River Lee', 8), ('River Dearne', 8), ('River Eden', 8), ('River Teme', 8), ('Great Stour', 8), ('River Welland', 8), ('River Exe', 8), ('River Swale', 7), ('River Tees', 7), ('River Kennet', 7), ('River Wandle', 7), ('River Thame', 7), ('River Leven', 7), ('River Ravensbourne', 7), ('River Yeo', 7), ('River Cam', 7), ('River Ure', 7), ('River Loddon', 7), ('Bristol Frome', 6), ('River Wyre', 6), ('River Tamar', 6), ('River Evenlode', 6), ('River North Tyne', 6), ('Kyd Brook', 6), ('River Mersey', 6), ('River Cole', 6), ('River Gaunless', 6), ('River Hull', 6), ('River Meon', 6), ('River Windrush', 5), ('River Nidd', 5), ('River Esk', 5), ('River Douglas', 5), ('River Ray', 5), ('River Stort', 5), ('River Tyne', 5), ('Horsbere Brook', 5), ('River Churnet', 5), ('River Roding', 5), ('River Soar', 5), ('River South Tyne', 5), ('River Ash', 5), ('River Wear', 5), ('River Tone', 5), ('River Lune', 5), ('River Axe', 5), ('River Chelt', 5), ('River Lark', 5), ('River Weaver', 5), ('River Brent', 5), ('Aire Washlands', 5), ('Beverley Brook', 5)]
