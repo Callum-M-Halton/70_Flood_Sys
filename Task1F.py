9cb3e058e44da6f47f8e5a3d6aa5df74b24d3026
Ki5hn <you@example.com>
2022-02-08 00:53:19 +0000

    More elegantly reworked the test_typical_range_consistent function

--- Task1F.py
+++ Task1F.py
@@ -11,7 +11,6 @@ def demonstrate_inconsistent_stations_list(stations = build_station_list()):
         name_list.append(station.name)
     name_list.sort()
     print(name_list)
-    assert name_list == ['Airmyn', 'Blacktoft', 'Braunton', 'Brentford', 'Broomfleet Weighton Lock', 'East Hull Hedon Road', 'Eastbourne Harbour', 'Fleetwood', 'Goole', 'Hedon Thorn Road Bridge', 'Hedon Westlands Drain', 'Hempholme Pumping Station Roam Drain', 'Hull Barrier Victoria Pier', 'Hull High Flaggs, Lincoln Street', 'Littlehampton', 'Medmerry', 'North America', 'Paull', 'Salt End', 'Sandwich Quay', 'Sindlesham Mill', 'Stone Creek', 'Templers Road', 'Tickton Pumping Station', 'Topsham', 'Totnes', 'Truro Harbour', 'Wilfholme Pumping Station']
 
 
 if __name__ == "__main__":
