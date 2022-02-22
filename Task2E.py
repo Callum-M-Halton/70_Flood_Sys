2860a1d5b3f0daea29f04868434b07dee13b307e
Ki5hn <you@example.com>
2022-02-22 15:02:26 +0000

    Completed 2E, 2F, adding in error checking for stations that have no levels or data providable

--- Task2E.py
+++ Task2E.py
@@ -1,23 +1,36 @@
+
 from floodsystem.plot import plot_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
 from datetime import datetime, timedelta
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
 
 
-def demonstrate_plot_water_levels():
+def demonstrate_plot_water_levels(n):
     """
     Plots the water levels for the 5 stations with the highest relative
     water level
     """
-    stations = build_station_list()[:5]
-    for station in stations:
+    all_stations = build_station_list()
+    update_water_levels(all_stations)
+    top_five_stations = stations_highest_rel_level(all_stations, n)
+    for station in top_five_stations:
         dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days = 10))
-        plot_water_levels(station, dates, levels)
-
+        if dates: #Ensures if a stations dates data doesnt exist, top 5 still print
+            if levels: #Ensures if stations level data doesn't exist, top 5 still print
+                plot_water_levels(station, dates, levels)
+            else: 
+                if len(top_five_stations) == 5:
+                    demonstrate_plot_water_levels(6)
+                    break
+        else:
+            if len(top_five_stations) == 5:
+                demonstrate_plot_water_levels(6)
+                break
 
 
 
 
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
-    demonstrate_plot_water_levels()
+    demonstrate_plot_water_levels(5)
--- Task2F.py
+++ Task2F.py
@@ -1,28 +1,42 @@
 
 from floodsystem.datafetcher import fetch_measure_levels
 from datetime import timedelta
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 import matplotlib.dates as date
 from floodsystem.plot import plot_water_levels_with_fit
+from floodsystem.flood import stations_highest_rel_level
 
 
 
 
-def demonstrate_poly_fit(t, d):
+def demonstrate_poly_fit(t, d, n):
     """
-    Creates a polynomial fit of degree d for the 5 stations at which the current relative
+    Creates a polynomial fit of degree d for the n stations at which the current relative
     water level is greatest, for a period of time extending back t days, 
     """
-    stations= build_station_list()
-    for station in stations:
-        dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days = t))
-        dates = date.date2num(dates)
-        plot_water_levels_with_fit(station, dates, levels, d)
-    
 
+    all_stations= build_station_list()
+    update_water_levels(all_stations)
+    top_five_stations = stations_highest_rel_level(all_stations, n)
+
+    for station in top_five_stations:
+        dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days = t))
+        if dates:
+            if levels: #Making sure they exist as sometimes they do not
+                dates = date.date2num(dates)
+                plot_water_levels_with_fit(station, dates, levels, d)
+            else:
+                if len(top_five_stations) == 5: #Ensures 5 stations are still printed even if the first one has no data
+                    demonstrate_poly_fit(2, 4, n+1)
+                    break
+        else: #Ensures 5 stations are still printed even if the first one has no data
+            if len(top_five_stations) == 5:
+                demonstrate_poly_fit(2, 4, n+1)
+                break
+                           
 
 
 
 if __name__ == "__main__":
     print("*** Task 2F: CUED Part IA Flood Warning System ***")
-    demonstrate_poly_fit(2, 4)
+    demonstrate_poly_fit(2, 4, 5)
