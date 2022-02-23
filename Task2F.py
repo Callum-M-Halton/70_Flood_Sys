292a75d19b68bca2e7bb9969523c37ae66941c7d
ki5hn <kmz24@cam.ac.uk>
2022-02-23 20:34:33 +0000

    fixes task2f

--- Task2F.py
+++ Task2F.py
@@ -1,42 +1,28 @@
 
 from floodsystem.datafetcher import fetch_measure_levels
 from datetime import timedelta
-from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.stationdata import build_station_list
 import matplotlib.dates as date
 from floodsystem.plot import plot_water_levels_with_fit
-from floodsystem.flood import stations_highest_rel_level
 
 
 
 
-def demonstrate_poly_fit(t, d, n):
+def demonstrate_poly_fit(t, d):
     """
-    Creates a polynomial fit of degree d for the n stations at which the current relative
+    Creates a polynomial fit of degree d for the 5 stations at which the current relative
     water level is greatest, for a period of time extending back t days, 
     """
-
-    all_stations= build_station_list()
-    update_water_levels(all_stations)
-    top_five_stations = stations_highest_rel_level(all_stations, n)
-
-    for station in top_five_stations:
+    stations= build_station_list()
+    for station in stations:
         dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days = t))
-        if dates:
-            if levels: #Making sure they exist as sometimes they do not
-                dates = date.date2num(dates)
-                plot_water_levels_with_fit(station, dates, levels, d)
-            else:
-                if len(top_five_stations) == 5: #Ensures 5 stations are still printed even if the first one has no data
-                    demonstrate_poly_fit(2, 4, n+1)
-                    break
-        else: #Ensures 5 stations are still printed even if the first one has no data
-            if len(top_five_stations) == 5:
-                demonstrate_poly_fit(2, 4, n+1)
-                break
-                           
+        dates = date.date2num(dates)
+        plot_water_levels_with_fit(station, dates, levels, d)
+    
+
 
 
 
 if __name__ == "__main__":
     print("*** Task 2F: CUED Part IA Flood Warning System ***")
-    demonstrate_poly_fit(2, 4, 5)
+    demonstrate_poly_fit(2, 4)
