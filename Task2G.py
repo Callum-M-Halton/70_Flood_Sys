a9fc5992e0d9d7d911fedfb8297877da6dca6ae3
Callum Halton <ch936@cam.ac.uk>
2022-02-23 23:23:52 +0000

    Completes Task2G

--- Task2G.py
+++ Task2G.py
@@ -9,19 +9,19 @@ def run():
     station_list = build_station_list()
     update_water_levels(station_list)
     towns_flood_times_ordered = get_towns_flood_times_ordered(station_list)
-    
-    previous_flood_time = math.inf
-    current_rank = 1
-    for town_pair in towns_flood_times_ordered:
+    #print(type(towns_flood_times_ordered))
+    previous_flood_time = -1
+    current_rank = 0
+    for town_pair in towns_flood_times_ordered[:100]:
+        #print(town_pair)
         flood_time = town_pair[1]
         risk_level = risk_level_from_flood_time(flood_time)
-
 
         if flood_time > previous_flood_time:
             previous_flood_time = flood_time
             current_rank += 1
         
-        print(f"{current_rank}.", f"{town_pair[0]}:", risk_level)
+        print(f"{current_rank}. {town_pair[0]}: {risk_level}, days-flood: ~{flood_time}")
 
 
 if __name__ == "__main__":
