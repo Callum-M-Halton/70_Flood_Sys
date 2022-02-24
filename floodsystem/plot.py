3b8277ceba0f2879c42b9cabd148072e72b6b3b7
Ki5hn <you@example.com>
2022-02-24 10:50:22 +0000

    small modifications to task 2f

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -28,8 +28,12 @@ def plot_water_levels_with_fit(station, dates, levels, p):
     Plots water level data points and a curve of best fit with degree p going through them
     """
     poly, d0 = polyfit(dates, levels, p)
-    plt.plot(dates-d0, poly(dates-d0))
-    plt.plot(dates-d0, levels, ".")
+    plt.plot(dates-d0, levels, ".", color = "b")
+    plt.plot(dates-d0, poly(dates-d0), color = "r")
+    
+    if station.typical_range_consistent(): 
+        plt.axhline(y=station.typical_range[1], linestyle = "--", color = "r") #Plots typical high line
+        plt.axhline(y = station.typical_range[0], linestyle = "--", color = "g") #PLots typical low line
     plt.xlabel("Days since 2 days ago")
     plt.xticks(rotation=45)
     plt.ylabel("Water level (M)")
